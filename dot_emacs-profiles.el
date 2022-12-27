58d131caeac6127d705b8f8636910c15ffa3aa30
Clay Kaufmann <claykaufmann@gmail.com>
2022-12-27 15:42:23 -0800

    add emacs profiles

---
+++ dot_emacs-profiles.el
@@ -0,0 +1,4 @@
+(("default"   . ((user-emacs-directory . "~/doom-emacs")))
+  ("legacy" . ((user-emacs-directory . "~/.emacs.legacy")))
+  ("doom-test" . ((user-emacs-directory . "~/doom-emacs-test"))))
+ 
