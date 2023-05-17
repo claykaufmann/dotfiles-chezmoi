b0b47cdb1b234ad072c89a5f20253335f12ad8dc
Clay Kaufmann <ckaufmann@beta.team>
2023-05-17 09:11:18 -0400

    update polybar

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -6,5 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# if we detect certain monitors, we want to activate home
+monitors=xrandr | grep " connected " | awk '{print$1}'
+
 (sleep 2; polybar bar --config=~/.config/polybar/config.ini) &
 (sleep 2; polybar external --config=~/.config/polybar/config.ini) &
+(sleep 2; polybar alt-external --config=~/.config/polybar/config.ini) &
+
